4b058c26afeef1294c6c9fc31662f38bd1b43182
Saori Sugiyama <sugiyama.saori.biz@gmail.com>
2017-05-22 14:07:26 +0800

    UI initialization

--- KtExample/app/src/main/java/com/astir_trotter/ktexample/ktexample/MainActivity.kt
+++ KtExample/app/src/main/java/com/astir_trotter/ktexample/ktexample/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.astir_trotter.ktexample.ktexample
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.view.Menu
-import android.view.MenuItem
+import com.astir_trotter.ktexample.ktexample.fragments.NewsFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,24 +14,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
-
-        if (id == R.id.action_settings) {
-            return true
+        if (savedInstanceState == null) {
+            changeFragment(NewsFragment())
         }
+    }
 
-        return super.onOptionsItemSelected(item)
+    fun changeFragment(f: Fragment, cleanStack: Boolean = false) {
+        val ft = supportFragmentManager.beginTransaction();
+        if (cleanStack) {
+            clearBackStack();
+        }
+        ft.setCustomAnimations(
+                R.anim.abc_fade_in, R.anim.abc_fade_out, R.anim.abc_popup_enter, R.anim.abc_popup_exit);
+        ft.replace(R.id.activity_base_content, f);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+    fun clearBackStack() {
+        val manager = supportFragmentManager;
+        if (manager.backStackEntryCount > 0) {
+            val first = manager.getBackStackEntryAt(0);
+            manager.popBackStack(first.id, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        }
+    }
+
+    override fun onBackPressed() {
+        val fragmentManager = supportFragmentManager;
+        if (fragmentManager.backStackEntryCount > 1) {
+            fragmentManager.popBackStack();
+        } else {
+            finish();
+        }
     }
 }
---
+++ KtExample/app/src/main/java/com/astir_trotter/ktexample/ktexample/fragments/NewsFragment.kt
@@ -0,0 +1,11 @@
+package com.astir_trotter.ktexample.ktexample.fragments
+
+import android.support.v4.app.Fragment
+
+/**
+ * @author          - Saori Sugiyama
+ * @contact         - sugiyama.saori.biz@gmail.com
+ * @date            - 5/21/17
+ */
+class NewsFragment: Fragment() {
+}
