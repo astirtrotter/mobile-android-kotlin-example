139ea0b606891afae7c8c5a57788dd5fdd25a604
Saori Sugiyama <sugiyama.saori.biz@gmail.com>
2017-05-22 22:36:22 +0800

    rest api integration

--- KtExample/app/src/main/java/com/astir_trotter/ktexample/features/news/NewsManager.kt
+++ KtExample/app/src/main/java/com/astir_trotter/ktexample/features/news/NewsManager.kt
@@ -1,37 +1,37 @@
 package com.astir_trotter.ktexample.features.news
 
+import com.astir_trotter.ktexample.api.RestAPI
 import com.astir_trotter.ktexample.commons.RedditNewsItem
 import rx.Observable
 
 /**
+ * News Manager allows you to request more news from Reddit.
+ *
  * @author          - Saori Sugiyama
  * @contact         - sugiyama.saori.biz@gmail.com
  * @date            - 5/22/17
  */
 
-/**
- * News Manager allows you to request more news from Reddit.
- *
- * @author juancho
- */
-class NewsManager {
+class NewsManager(private val api: RestAPI = RestAPI()) {
 
-    fun getNews(): Observable<List<RedditNewsItem>> {
+    fun getNews(limit: String = "10"): Observable<List<RedditNewsItem>> {
         return Observable.create {
             subscriber ->
 
-            val news = mutableListOf<RedditNewsItem>()
-            for (i in 1..10) {
-                news.add(RedditNewsItem(
-                        "author$i",
-                        "Title $i",
-                        i, // number of comments
-                        1457207701L - i * 200, // time
-                        "http://lorempixel.com/200/200/technics/$i", // image url
-                        "url"
-                ))
+            val callResponse = api.getNews("", limit)
+            val response = callResponse.execute()
+
+            if (response.isSuccessful) {
+                val news = response.body().data.children.map {
+                    val item = it.data
+                    RedditNewsItem(item.author, item.title, item.num_comments,
+                            item.created, item.thumbnail, item.url)
+                }
+                subscriber.onNext(news)
+                subscriber.onCompleted()
+            } else {
+                subscriber.onError(Throwable(response.message()))
             }
-            subscriber.onNext(news)
         }
     }
 }
