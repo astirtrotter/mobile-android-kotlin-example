3cc4bbdfa2672b9248eb42a0bc6d98d635e07998
Saori Sugiyama <sugiyama.saori.biz@gmail.com>
2017-05-23 15:08:42 +0800

    make parcel able by plugin

--- KtExample/app/src/main/java/com/astir_trotter/ktexample/commons/Models.kt
+++ KtExample/app/src/main/java/com/astir_trotter/ktexample/commons/Models.kt
@@ -4,7 +4,6 @@ import android.os.Parcel
 import android.os.Parcelable
 import com.astir_trotter.ktexample.commons.adapter.AdapterConstants
 import com.astir_trotter.ktexample.commons.adapter.ViewType
-import com.astir_trotter.ktexample.commons.extensions.createParcel
 
 /**
  * @author          - Saori Sugiyama
@@ -15,27 +14,25 @@ data class RedditNews(
         val after: String,
         val before: String,
         val news: List<RedditNewsItem>) : Parcelable {
-
     companion object {
-        @JvmField @Suppress("unused")
-        val CREATOR = createParcel { RedditNews(it) }
+        @JvmField val CREATOR: Parcelable.Creator<RedditNews> = object : Parcelable.Creator<RedditNews> {
+            override fun createFromParcel(source: Parcel): RedditNews = RedditNews(source)
+            override fun newArray(size: Int): Array<RedditNews?> = arrayOfNulls(size)
+        }
     }
 
-    protected constructor(parcelIn: Parcel) : this(
-            parcelIn.readString(),
-            parcelIn.readString(),
-            mutableListOf<RedditNewsItem>().apply {
-                parcelIn.readTypedList(this, RedditNewsItem.CREATOR)
-            }
-    )
+    constructor(source: Parcel) : this(
+            source.readString(),
+            source.readString(),
+            source.createTypedArrayList(RedditNewsItem.CREATOR))
+
+    override fun describeContents() = 0
 
     override fun writeToParcel(dest: Parcel, flags: Int) {
         dest.writeString(after)
         dest.writeString(before)
         dest.writeTypedList(news)
     }
-
-    override fun describeContents() = 0
 }
 
 data class RedditNewsItem(var author: String,
@@ -48,8 +45,10 @@ data class RedditNewsItem(var author: String,
     override fun getViewType() = AdapterConstants.NEWS
 
     companion object {
-        @JvmField @Suppress("unused")
-        val CREATOR = createParcel { RedditNewsItem(it) }
+        @JvmField val CREATOR: Parcelable.Creator<RedditNewsItem> = object : Parcelable.Creator<RedditNewsItem> {
+            override fun createFromParcel(source: Parcel): RedditNewsItem = RedditNewsItem(source)
+            override fun newArray(size: Int): Array<RedditNewsItem?> = arrayOfNulls(size)
+        }
     }
 
     constructor(source: Parcel) : this(
@@ -58,18 +57,16 @@ data class RedditNewsItem(var author: String,
             source.readInt(),
             source.readLong(),
             source.readString(),
-            source.readString()
-    )
+            source.readString())
 
-    override fun writeToParcel(dest: Parcel?, flags: Int) {
-        dest?.writeString(author)
-        dest?.writeString(title)
-        dest?.writeInt(numComments)
-        dest?.writeLong(created)
-        dest?.writeString(thumbnail)
-        dest?.writeString(url)
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(author)
+        dest.writeString(title)
+        dest.writeInt(numComments)
+        dest.writeLong(created)
+        dest.writeString(thumbnail)
+        dest.writeString(url)
     }
-
-    override fun describeContents(): Int = 0
-
 }
--- KtExample/app/src/main/java/com/astir_trotter/ktexample/commons/extensions/Extensions.kt
+++ KtExample/app/src/main/java/com/astir_trotter/ktexample/commons/extensions/Extensions.kt
@@ -29,10 +29,3 @@ fun ImageView.loadImg(imageUrl: String) {
         Picasso.with(context).load(imageUrl).into(this)
     }
 }
-
-// Inline function to create Parcel Creator
-inline fun <reified T : Parcelable> createParcel(
-        crossinline createFromParcel: (Parcel) -> T?): Parcelable.Creator<T> = object : Parcelable.Creator<T> {
-            override fun createFromParcel(source: Parcel): T? = createFromParcel(source)
-            override fun newArray(size: Int): Array<out T?> = arrayOfNulls(size)
-        }
